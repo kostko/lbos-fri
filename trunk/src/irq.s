f9a15b79b784e3e2d98ecbf794aa93009479fee7
kostko <kostko@4fa3629e-ac03-11dd-a5cd-697bfd0a0cef>
2008-11-06 18:56:15 +0000

    Basic PIT stuff.

--- trunk/src/irq.s
+++ trunk/src/irq.s
@@ -3,7 +3,33 @@
  * general OS framework (C) 2008 by FRI/OS1/Group8
  */
 .global sys_irq_handler
+.global svc_newtask
+
+/* Include structure definitions and static variables */
+.include "include/at91sam9260.s"
 
 sys_irq_handler:
   /* System controller interrupt handler */
-  b sys_irq_handler
+  sub r14, r14, #4
+  stmfd r13!, {r0-r12,r14}
+  
+  /* Check if PIT is responsible for this interrupt (since
+      SYSC IRQ channel is shared) */
+  ldr r0, =PIT_BASE
+  ldr r1, [r0, #PIT_SR]
+  tst r1, #1
+  beq __pit_irq_handler
+  
+  /* Nothing matched */
+
+  /* Signal AIC end of interrupt and return */
+  ldr r0, =AIC_BASE
+  str r0, [r0, #AIC_EOICR]
+  
+  ldmfd r13!, {r0-r12,pc}^
+
+__pit_irq_handler:
+  /* PIT handler, just switch to next task */
+  ldr r0, =AIC_BASE
+  str r0, [r0, #AIC_EOICR]
+  b svc_newtask
--- trunk/src/syscall.s
+++ trunk/src/syscall.s
@@ -3,7 +3,16 @@
  * general OS framework (C) 2008 by FRI/OS1/Group8
  */
 .global sycall_handler
+.global svc_newtask
 
 sycall_handler:
   /* System call handler/dispatcher */
   b syscall_handler
+
+/* ================================================================
+                       SYSTEM CALLS GO HERE
+   ================================================================
+*/
+svc_newtask:
+  /* New task syscall */
+  b svc_newtask
