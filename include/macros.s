3ab0d9dd17b67b6749230c25c457b7bb4a243225
Jernej Kos <jernej@kos.mx>
2008-11-07 18:47:18 +0000

    Added LED_ON and LED_OFF macros.

--- include/macros.s
+++ include/macros.s
@@ -13,3 +13,15 @@
   bic r0, r0, #1 << 7   /* Clear IRQ disable bit (7) */
   msr cpsr, r0          /* Write r0 to CPSR */
 .endm
+
+.macro LED_ON
+  ldr r0, =PIOC_BASE
+  mov r1, #1 << 1
+  strne r1, [r0, #PIO_CODR]
+.endm
+
+.macro LED_OFF
+  ldr r0, =PIOC_BASE
+  mov r1, #1 << 1
+  strne r1, [r0, #PIO_SODR]
+.endm
