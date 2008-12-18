88ba73dcd48c91a21ef0c46d3bd5e44b30129a77
gasper.rupnik <gasper.rupnik@4fa3629e-ac03-11dd-a5cd-697bfd0a0cef>
2008-12-18 09:48:44 +0000

    translated our comments

--- branches/group3/src/svc_delay.s
+++ branches/group3/src/svc_delay.s
@@ -26,59 +26,59 @@ dl_arfd:
   /* Load current task TCB pointer */
   LOAD_CURRENT_TCB r1
           
-  ldr   r2, =DLYLIST                    /* r2 -> naslov prvega DLY bloka */  
+  ldr   r2, =DLYLIST                    /* r2 -> address of first DLY block */  
    
   mov r6, r0                                  
-  bl irq_disable						/* onemogocimo prekinitve */
+  bl irq_disable						/* disable interruption */
   mov r7, r0
   mov r0, r6
           
-  add   r4, r2, #72                     /* r4 -> naslov na zadnjega v DLYLIST */
-  ldr   r5, [r4, #D_TCB]				/* r5 -> kazalec na TCB zadnjega v vrsti */
-  cmp   r5, #0							/* ali je mesto zasedeno? */
-  bne   __dla_ovrflw				    /* mesto je zasedeno -> NAPAKA */
+  add   r4, r2, #72                     /* r4 -> address of last element in DLYLIST */
+  ldr   r5, [r4, #D_TCB]				/* R5 -> pointer to the TCB latest in a list */
+  cmp   r5, #0							/* check if FREE */
+  bne   __dla_ovrflw				    /* OVERFLOW_ERROR */
           
 __dla_next:    
-  ldr   r3, [r2]                        /* vrednost na prvem mestu v tabeli */    
-  cmp   r3, #0                          /* ali je kaksen blok ze v seznamu? */
-  beq   __dla_emlst                     /* skoci naprej, ce ni nobenega */
+  ldr   r3, [r2]                        /* value in the first place in the table */    
+  cmp   r3, #0                          /* Is the list empty? */
+  beq   __dla_emlst                     /* jump if empty */
           
-  ldr   r3, [r2, #D_TOUT]               /* preberi delay prvega v seznamu */
-  cmp   r0, r3                          /* primerjas s trenutnim delay-em */
-  ble   __dla_addl                      /* ce je manjse ali enako */
+  ldr   r3, [r2, #D_TOUT]               /* read the first delay in the list */
+  cmp   r0, r3                          /* compare with the current delay */
+  ble   __dla_addl                      /* if less or equal */
 
-  /* ce je vecji */          
-  sub   r0, r0, r3                      /* odstejes svoj delay */
-  add   r2, r2, #8                      /* gres na naslednjega */
+  /* if greater */          
+  sub   r0, r0, r3                      /* substract  delay */
+  add   r2, r2, #8                      /* go to next */
   b     __dla_next
 
-  /* ce je manjse ali enako */                 
+  /* if less or equal */                 
 __dla_addl:    
-  mov   r4, r2                          /* v pomozno shrani */
+  mov   r4, r2                          /* save to temp */
   
 __dla_srend:   
-  add   r4, r4, #8                      /* hodi naprej */
-  ldr   r5, [r4, #D_TCB]                /* nalozi vrednost z naslova r4 */
-  cmp   r5, #0                          /* primerjaj z 0 - a si prsu do konca */
-  bne   __dla_srend                     /* ce nismo se na koncu gremo v zanko */
+  add   r4, r4, #8                      /* go to next */
+  ldr   r5, [r4, #D_TCB]                /* load value from r4 */
+  cmp   r5, #0                          /* compare to 0 - are we finished? */
+  bne   __dla_srend                     /* if we were to go at the end of the loop */
   
 __dla_next2:
-  ldr   r5, [r4, #-8]                   /* v pomoznega (r5) shrani TCB predhodnega v tabeli */
-  str   r5, [r4, #D_TCB]                /* shrani r5 v TCB trenutnega v tabeli */
-  ldr   r5, [r4, #-4]                   /* v pomoznega (r5) shrani TOUT predhodnega v tabeli */
-  sub   r4, r4, #8                      /* gres za enga nazaj */
-  cmp   r4, r2                          /* ali smo ze na pravem mestu za vstavljanje? */
-  subeq r5, r5, r0                      /* odstej mu njegov TOUT za TOUT tega, ko hoces dodati */
-  str   r5, [r4, #D_TOUT + 8]           /* shrani r5 v TOUT trenutnega v tabeli */
-  bne   __dla_next2                     /* ce se nismo na pravem mestu ponavljamo */
+  ldr   r5, [r4, #-8]                   /* in the temp (R5) saves the previous TCB in the table */
+  str   r5, [r4, #D_TCB]                /* store r5  in current TCB in the table */
+  ldr   r5, [r4, #-4]                   /* in temp(r5) store TOUT of the previous in the table */
+  sub   r4, r4, #8                      /* go one back */
+  cmp   r4, r2                          /* are we on the right place to insert? */
+  subeq r5, r5, r0                      /* substract it's TOUT for the value of TOUT of the one we want to insert */
+  str   r5, [r4, #D_TOUT + 8]           /* store r5 in TOUT of the current in the table */
+  bne   __dla_next2                     /* If not in the right place repeat */
                    
-  /* ce smo ze na pravem mestu ali pa je DLYLIST prazen */          
+  /* if in the rigt place or the DLYLIST is empty */          
 __dla_emlst: 
-  str   r1, [r2, #D_TCB]                /* shrani TCB na pravo mesto v DLYLIST */
-  str   r0, [r2, #D_TOUT]               /* shrani TOUT na pravo mesto v DLYLIST */
+  str   r1, [r2, #D_TCB]                /* store TCB in the right place in DLYLIST */
+  str   r0, [r2, #D_TOUT]               /* store TOUT in the right place in DLYLIST */
  
 __dld_tcwait:                                   
-  mov r0, r7							/* omogocimo prekinitve */
+  mov r0, r7							/* enable interruption */
   bl irq_restore						
 		  
 
@@ -86,22 +86,22 @@ __dld_tcwait:
  * Deleting requirements for delay
  */
 dl_drfd: 
-  bl irq_disable						/* onemogocimo prekinitve */
+  bl irq_disable						/* disable interruption */
   mov r7, r0
           
-  ldr   r2, =DLYLIST                    /* r2 -> naslov prvega DLY bloka */  
-  add   r4, r2, #80                     /* r4 -> naslov izven DLYLIST */
-  ldr   r1, [r2, #D_TCB]                /* r1 -> TCB prvega DLY bloka */
-  cmp   r1, #0                          /* ali je kaksen blok v seznamu? */
-  beq   __dld_exit                      /* skoci na konec, ce ni nobenega */
-  ldr   r0, [r2, #D_TOUT]               /* r0 -> TOUT prvega DLY bloka */
-  cmp   r0, #0                          /* ali je kaj za steti? */ 
-  ldreq r6, [r1, #T_FLAG]               /* preberi vrstico z zastavicami iz TCB-ja */
-  biceq r6, r6, #TWAIT                  /* izbrisi TWAIT zastavico */
-  streq r6, [r1, #T_FLAG]               /* shrani vrstico z zastavicami v TCB */
-  beq   __dld_nocnt                     /* skoci naprej, ce ni treba stetja */
+  ldr   r2, =DLYLIST                    /* r2 -> address of the first DLY block */  
+  add   r4, r2, #80                     /* r4 -> address out of DLYLIST */
+  ldr   r1, [r2, #D_TCB]                /* r1 -> TCB of the first DLY block */
+  cmp   r1, #0                          /* Is there any block in the list? */
+  beq   __dld_exit                      /* jump to the end, if none in the list */
+  ldr   r0, [r2, #D_TOUT]               /* r0 -> TOUT of the first DLY block */
+  cmp   r0, #0                          /* Is there anything to count? */ 
+  ldreq r6, [r1, #T_FLAG]               /* read the flags from TCB */
+  biceq r6, r6, #TWAIT                  /* remove TWAIT flag */
+  streq r6, [r1, #T_FLAG]               /* store the flags to TCB */
+  beq   __dld_nocnt                     /* jump forward, if nothing to count */
 
-  /* timer steje */          	  
+  /* timer count*/          	  
   ldr r6, =TC0_BASE  
   ldr r5, =CDLYTCB
   ldr r5, [r5]
@@ -119,52 +119,52 @@ __dld_firsts:
         
   /* Mark tasks undispatchable */
 __dld_mrknw:
-  ldr   r6, [r1, #T_FLAG]               /* preberi vrstico z zastavicami iz TCB-ja */
-  orr   r6, r6, #TWAIT                  /* postavi TWAIT zastavico */
-  str   r6, [r1, #T_FLAG]               /* shrani vrstico z zastavicami v TCB */
-  add   r2, r2, #8                      /* hodi naprej */
-  cmp   r2, r4                          /* ali smo ze izven DLYLIST-a? */
-  beq   __dld_nocnt                     /* smo izven DLYLIST-a */
-  ldr   r1, [r2, #D_TCB]                /* r1 -> TCB naslednjega DLY bloka */
-  cmp   r1, #0                          /* primerjaj z 0 - a si prsu do konca */
-  bne   __dld_mrknw                     /* ce se nisi na koncu nadaljuj z postavljanjem */
+  ldr   r6, [r1, #T_FLAG]               /* read the flags from TCB */
+  orr   r6, r6, #TWAIT                  /* set TWAIT flag */
+  str   r6, [r1, #T_FLAG]               /* store the flags to TCB */
+  add   r2, r2, #8                      /* go to next */
+  cmp   r2, r4                          /* are we out of DLYLIST? */
+  beq   __dld_nocnt                     /* We are out of DLYLIST */
+  ldr   r1, [r2, #D_TCB]                /* r1 -> TCB of the next DLY block */
+  cmp   r1, #0                          /* compare to 0 - are we finished */
+  bne   __dld_mrknw                     /* if not continue with setting */
                 
-  /* premeci vse v DLYLIST-u za ena v levo (nazaj) */
+  /* shift left every thing in DLYLIST */
 __dld_nocnt: 
-  ldr   r2, =DLYLIST                    /* r2 -> naslov prvega DLY bloka */ 
+  ldr   r2, =DLYLIST                    /* r2 -> address of the first DLY block */ 
   
 __dld_dia:
-  add   r2, r2, #8                      /* pomakni se na naslednji DLY blok */
-  cmp   r2, r4                          /* ali smo ze izven DLYLIST-a? */
-  beq   __dld_owf                       /* smo izven DLYLIST-a */
+  add   r2, r2, #8                      /* move to next DLY block */
+  cmp   r2, r4                          /* Are we out of DLYLIST? */
+  beq   __dld_owf                       /* We are out of DLYLIST */
           
-  ldr   r5, [r2, #D_TCB]                /* r5 -> TCB trenutnega DLY bloka */
-  cmp   r5, #0                          /* ali je veljaven trenutni DLY blok? */
-  beq   __dld_wblock                    /* trenutni DLY blok ni veljaven */
+  ldr   r5, [r2, #D_TCB]                /* r5 -> TCB of the current DLY block */
+  cmp   r5, #0                          /* is the current DLY block valid? */
+  beq   __dld_wblock                    /* current DLY block is not valid */
           
-  /* trenutni DLY blok je veljaven -> kopiraj ga za eno nazaj */
-  str   r5, [r2, #-8]                   /* r5 shrani v TCB prejsnega DLY bloka */
-  ldr   r5, [r2, #D_TOUT]               /* r5 -> TOUT trenutnega DLY bloka */
-  str   r5, [r2, #-4]                   /* r5 shrani v TOUT prejsnega DLY bloka */
-  b     __dld_dia                       /* pojdi naprej */         
+  /* current DLY block is valid -> copy it to previous place in the list */
+  str   r5, [r2, #-8]                   /* store r5  in the TCB of the previous DLY block */
+  ldr   r5, [r2, #D_TOUT]               /* r5 -> TOUT of the current DLY bloka */
+  str   r5, [r2, #-4]                   /* store r5 to TOUT of the previous DLY bloka */
+  b     __dld_dia                       /* go to next */         
           
-  /* smo izven DLYLIST-a */
+  /* We are out of DLYLIST */
 __dld_owf: 
   mov   r5, #0
 
-  /* trenutni DLY blok ni veljaven (0) -> zapisi 0 tudi v prejsnega */
+  /* current DLY block is not valid (0) -> write 0 in the previous */
 __dld_wblock:   
-  str   r5, [r2, #-8]                   /* zapisi 0 v TCB prejsnega DLY bloka */
-  str   r5, [r2, #-4]                   /* zapisi 0 v TOUT prejsnega DLY bloka */        
+  str   r5, [r2, #-8]                   /* write 0 into TCB of the previous DLY block */
+  str   r5, [r2, #-4]                   /* write 0 into  TOUT of the previous DLY block */       
           
-  mov r0, r7							/* omogocimo prekinitve */
+  mov r0, r7							/* enable interruption */
   bl irq_restore
            
-  b     dl_drfd                         /* ponovi za naslednjega */
+  b     dl_drfd                         /* repeat for  next */
 
-  /* koncano */
+  /* finished */
 __dld_exit:   
-  mov r0, r7							/* omogocimo prekinitve */
+  mov r0, r7							/* enable interrupt */
   bl irq_restore
  
   /* Switch to some other task */
@@ -172,7 +172,7 @@ __dld_exit:
   b     svc_newtask
           
 __dla_ovrflw:   
-  mov r0, r7							/* omogocimo prekinitve */
+  mov r0, r7							/* enable interrupt */
   bl irq_restore 
 
   /* Switch to some other task */
