f44c42da90fc26f70f043cf0b4175c6a17be5290
verbole <verbole@4fa3629e-ac03-11dd-a5cd-697bfd0a0cef>
2009-01-08 09:31:25 +0000

    drugi commit

--- branches/group5/src/directories.s
+++ branches/group5/src/directories.s
@@ -24,13 +24,13 @@ SVC_MKDIR:
 	cmp r2, #0			/* preverimo ce je naslov enak 0 - smo na koncu*/
 	bne OK				/* ERROR  */
 NO_DIR:	ldr r0, =E_NO_DIR
-		j KONEC
+		b KONEC
 	
 OK:	ldr r4, [r2]
 	str r4, [r3]			/* RLIST pa posodobimo na naslednji prazen dir */
 
-	ldr r10, =CURRENT_PROC		/*current od trenutnega procesa*/ 
-	ldr r10, [r10, #CURRENT_DIR]  	/*naslov trenutnega direktorija*/
+	ldr r10, =CURRENT		/*current od trenutnega procesa*/ 
+	ldr r10, [r10, #T_CURDIR]  	/*naslov trenutnega direktorija*/
 	str r10, [r2, #D_PARENT]	/* praznemu direktoriju nastavimo pointer na oceta */
 	str r1, [r2, #D_NAME]		/* nastavimo ime novoustvarjenemu direktoriju */
 	mov r4, #0
@@ -90,8 +90,8 @@ SVC_REMDIR:
 	*/
 
 	
-	ldr r10, =CURRENT_PROC		/*current od trenutnega procesa*/ 
-	ldr r10, [r10, #CURRENT_DIR]  	/*naslov trenutnega direktorija*/
+	ldr r10, =CURRENT		/*current od trenutnega procesa*/ 
+	ldr r10, [r10, #T_CURDIR]  	/*naslov trenutnega direktorija*/
 	ldr r5, [r10, #D_CHILD_T]		/* naslov current tabele otrok*/
 	ldr r8, [r10, #D_CHILD_T]		/* zapomnimo si zadnji obhod -> ce brisemo tudi tabelo, je potrebno zbrisati tudi kaalec prejsnje tabele */
 	cmp r5, #0
@@ -127,10 +127,11 @@ NAJDEN:					/* nasli smo svojega za brisanje */
 	cmp r6, #0			/* ali ima sinove */
 	bne	CHILD_EXIST_ERR
 BRISI_VSE:
-	str #0, [r5, #D_NAME]			/* brisemo ime */
-	str #0, [r5, #D_TYPE]		/* brisemo tip */
-	str #0, [r5, #D_PARENT]		/* brisemo fotra */
-	str #0, [r5, #D_CHILD_T]		/* brisemo otroke */
+	sub r11,r11,r11					/* v r11 shranimo nièlo */
+	str r11, [r5, #D_NAME]			/* brisemo ime */
+	str r11, [r5, #D_TYPE]		/* brisemo tip */
+	str r11, [r5, #D_PARENT]		/* brisemo fotra */
+	str r11, [r5, #D_CHILD_T]		/* brisemo otroke */
 	
 BACK_TO_DIRLIST:
 	ldr r3, =DIRLIST
@@ -161,26 +162,30 @@ PONOVI3:					/* iscemo zadnjega, da ga prestavimo na mesto zbrisanega */
 ZADNJI_X:					/* prestavljanje zadnjega, ce je le ta edini v tabeli -> potrebno je brisanje tabele */
 	sub r6, r6, #8			/* v r6 imamo naslov zadnjega */
 	str r6, [r5]
-	str #0, [r6]			/* zbrisemo mesto v tabeli, kjer se je nahajal zadnji */
+	sub r11,r11,r11					/* v r11 shranimo nièlo */
+	str r11, [r6]			/* zbrisemo mesto v tabeli, kjer se je nahajal zadnji */
 	
-	str #0, [r6, #C_CHILD1]			/* brisemo tabelo */
-	str #0, [r6, #C_CHILD2]	
-	str #0, [r6, #C_CHILD3]	
-	str #0, [r6, #C_CHILD_T]
+	sub r11,r11,r11					/* v r11 shranimo nièlo */
+	str r11, [r6, #C_CHILD1]			/* brisemo tabelo */
+	str r11, [r6, #C_CHILD2]	
+	str r11, [r6, #C_CHILD3]	
+	str r11, [r6, #C_CHILD_T]
 	
 	ldr r3, =DIRLIST		/* tabelo vrnemo v DIRLIST */ 
 	ldr r4, =DIRLIST
 	str r3, [r6]			/* nastavimo pointer izbrisanega dira na naslednjega praznega !!!!!!!!!!!!!!!!!! ->	ERROR???*/
 	str r6, [r3]			/* popravimo DIRLIST, da kaze na r6 - praznega */
 	
-	str #0, [r8]			/* brisanje kazalca na naslednjo tabelo */
+	str r11, [r8]			/* brisanje kazalca na naslednjo tabelo */
 	b	KONEC
 	
 
 ZADNJI:						/* prestavljanje zadnjega, ce ni edini v tabeli */
 	sub r6, r6, #8			/* v r6 imamo naslov zadnjega */
 	str r6, [r5]
-	str #0, [r6]			/* zbrisemo mesto v tabeli, kjer se je nahajal zadnji */
+	
+	sub r11,r11,r11					/* v r11 shranimo nièlo */
+	str r11, [r6]			/* zbrisemo mesto v tabeli, kjer se je nahajal zadnji */
 	
 	b	KONEC
 	
@@ -213,8 +218,7 @@ SVC_CHGDIR:
 	/* ce so v registrih imena direktorijev, je treba najprej dobiti njihove naslove !!!!!!!!!!!!!!!!!!!!!! */
 
 
-	ldr r5, =CURRENT
-	ldr r6, [r5, #T_CURDIR]
+	ldr r6, =D_ROOT
 	ldr r6, [r6, #D_CHILD_T]
 	
 	cmp r1, #0
@@ -267,6 +271,80 @@ END_CHILD_T_ERR:
 	b	KONEC
 
 
+/* ================================================================	
+                           SVC_OPENF
+   ================================================================
+*/		
+SVC_OPENF:
+	bl SUB_SEARCH
+	b SVC_OPEN
+
+	
+/* ================================================================
+                           SVC_DELF
+   ================================================================
+*/		
+SVC_DELF:
+	bl SUB_SEARCH
+	b SVC_DEL
+
+	
+/* ================================================================
+                           SVC_APPENDF
+   ================================================================
+*/		
+SVC_APPENDF:
+	bl SUB_SEARCH
+	b SVC_APPEND	
+	
+	
+/* ================================================================
+                           SUBRUTINE SEARCH
+   ================================================================
+*/	
+
+/* V r0 dobimo ime datoteke, ki jo moramo poiskati in v r0 podati naslov prvega clustra. */
+
+SUB_SEARCH:
+
+	ldr r6, =D_ROOT
+	ldr r6, [r6, #D_CHILD_T]
+	
+	cmp r1, #0
+	beq NO_ATR_ERR
+	
+SEARCH1:						/*zacnemo z iskanjem po tabeli z naslovi (sprehod 3x) */
+	ldr r7, [r6,#D_NAME]
+	cmp r7,r1
+	beq NAJDENO1
+	cmp r7, #-1				/* check if child table has no more entries */
+	beq END_CHILD_T_ERR
+	add r6, r6, #4
+	
+	ldr r7, [r6,#D_NAME]
+	cmp r7,r1
+	beq NAJDENO1
+	cmp r7, #-1
+	beq END_CHILD_T_ERR
+	add r6, r6, #4
+	
+	ldr r7, [r6,#D_NAME]
+	cmp r7,r1
+	beq NAJDENO1
+	cmp r7, #-1
+	beq END_CHILD_T_ERR
+
+						/*ce ne najdemo pravega se premaknemo na naslednjo tabelo sinov ter ponovimo search */
+	add r6, r6, #4
+	ldr r6, [r6]
+	b SEARCH1
+	
+NAJDENO1:						/*element je najden - premaknemo se globje v strukturi, v r6 imamo naslov pravega dira */
+	ldr r0, [r6, #D_TYPE]
+	mov r15,r14 				/* shranimo povratni naslov */
+
+
+
 /* ================================================================
                            SYSTEM CALL DIR UP
    ================================================================
@@ -285,3 +363,21 @@ SVC_DIRUP:
 KONEC:
 	/* Switch to some other task */
 	b svc_newtask
+	
+	
+	
+/*	INIT:
+ treba se je sprehodit po strukturi in nastavit kazalce, zadnjega pa na 0.
+
+	TODO!!!
+	INIT metoda!!! - current, dirlist (name->name...->name->0), nastavi root,
+	child table ->morajo bit nièle
+	remdir treba nastavit to sranje
+	disable interrupts
+	mkdir remdir za 6 skupino
+	current proc + current dir pa te fore
+	inlcudi pa to sranje
+	dir up ne preverja ce je ze na vrhu
+	chdir se sprehaja od currenta dol, ne od roota...
+	
+*/
