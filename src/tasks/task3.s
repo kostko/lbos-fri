8332e4c8c526a73f7da9a6966f896ca39715c77a
ukosic <ukosic@4fa3629e-ac03-11dd-a5cd-697bfd0a0cef>
2009-01-08 17:09:48 +0000

    Changed tasks. SYS_SIGNAL SVC number is not valid! [000CE296]

--- src/tasks/task3.s
+++ src/tasks/task3.s
@@ -1,16 +1,25 @@
 .section task_code, "ax"
 .code 32
 
+/* Include structure definitions and static variables */
+.include "include/structures.s"
+
 mov r0, #1
 mov r1, #1
 
-swi SYS_WAIT
+swi #SYS_WAIT
 
-mov r10, #3
+ldr r10, TESTBUF
+str r10, MEM
+ldr r10, MEM
+add r10, r10, #12
+str r10, ID
 
 mov r0, #2
 mov r1, #1
-swi SYS_SIGNAL
+swi #SYS_SIGNAL
+
+swi #SYS_EXIT
 
 
 /*************************************************************
@@ -19,4 +28,6 @@ swi SYS_SIGNAL
 .section task_data, "aw"
 /* Per-task data structures may be defined below */
 
-TESTBUF: .asciz "Hello"
+TESTBUF: .asciz "www"
+ID: .asciz "3"
+MEM: .space 20
--- src/tasks/task4.s
+++ src/tasks/task4.s
@@ -1,12 +1,21 @@
 .section task_code, "ax"
 .code 32
 
+/* Include structure definitions and static variables */
+.include "include/structures.s"
+
 mov r0, #2
 mov r1, #1
 
-swi SYS_WAIT
+swi #SYS_WAIT
 
-mov r10, #4
+ldr r10, TESTBUF
+str r10, MEM
+ldr r10, MEM
+add r10, r10, #12
+str r10, ID
+
+swi #SYS_EXIT
 
 
 /*************************************************************
@@ -15,4 +24,6 @@ mov r10, #4
 .section task_data, "aw"
 /* Per-task data structures may be defined below */
 
-TESTBUF: .asciz "Hello"
+TESTBUF: .asciz "www"
+ID: .asciz "4"
+MEM: .space 20
