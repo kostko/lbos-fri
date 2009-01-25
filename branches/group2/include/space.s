63701eac58a3256644fcae2edac74e19391d6542
ukosic <ukosic@4fa3629e-ac03-11dd-a5cd-697bfd0a0cef>
2009-01-25 17:34:20 +0000

    Now all tasks work.

--- branches/group2/include/space.s
+++ branches/group2/include/space.s
@@ -13,7 +13,7 @@ TINDEX: .space 4    /* Current task index */
 TCBLIST: .space 4   /* Pointer to list of TCBs */
 CURRENT: .space 4   /* Pointer to current task's TCB */
 MCBLIST: .space 4   /* Pointer to free list of MCBs */
-	
+
 /* Task TCB placeholders */
 .align 2
 TASK1: .space TCBSIZE
@@ -36,7 +36,7 @@ MCBAREA:  .space MCBSIZE*NMCBS
 /* Task initialization data (see main.s/task_init) */
 .align 2
 TASK_INITDATA:
-      /* TCB | Status register | Task address  | Task size in pages (currently statically defined)
+      /* TCB | Status register | Task address  | Task size in pages (currently statically defined)*/ 
 .long TASK1,   PSR_MODE_USER,    _task1_paddr,   256*1024 / PAGESIZE
 .long TASK2,   PSR_MODE_USER,    _task2_paddr,   256*1024 / PAGESIZE  
 .long TASK3,   PSR_MODE_USER,    _task3_paddr,   256*1024 / PAGESIZE
@@ -47,22 +47,21 @@ TASK_INITDATA:
 .long TASK8,   PSR_MODE_USER,    _task8_paddr,   256*1024 / PAGESIZE
 .long 0
 
-
 /* WARNING ABOUT ADDING NEW TASKS
-*
-* If you wish to add more tasks, you need to edit the linker script and
-* define a new task section. Note that all task sections must be aligned
-* to proper 256K boundaries! Also don't forget to increment MAXTASK in
-* globsls.s otherwise tasks will not be scheduled.
-*/
+ *
+ * If you wish to add more tasks, you need to edit the linker script and
+ * define a new task section. Note that all task sections must be aligned
+ * to proper 256K boundaries! Also don't forget to increment MAXTASK in
+ * globsls.s otherwise tasks will not be scheduled.
+ */
 
 
 /* Timer structures */
 .align 2
-TIMERAREA: .space TMSIZE*MAXTASK /* Area for timer alocation */
-TIMERFREE: .space 4 /* Pointer to list of free timers */
-TIMERQUEUE: .space 4 /* Timer queue pending firing */
-CUR_JIFFIES: .space 4 /* Current jiffies value */
+TIMERAREA: .space TMSIZE*MAXTASK  /* Area for timer alocation */
+TIMERFREE: .space 4               /* Pointer to list of free timers */
+TIMERQUEUE: .space 4              /* Timer queue pending firing */
+CUR_JIFFIES: .space 4             /* Current jiffies value */
 
 /* Terminal I/O structures */
 .align 2
@@ -74,7 +73,7 @@ PAGEBITMAP: .space MAXPAGES/8, 255
 PAGEOFFSET: .long __PAGE_OFFSET__
 
 /* Kernel stacks (Supervisor and IRQ modes have separate stacks). For
-memory locations see layout.ind linker script! */
+   memory locations see layout.ind linker script! */
 .align 2
 STACK_SUPM_END: .long __STACK_END__ - STACK_SIZE*4
 STACK_IRQM_END: .long __STACK_END__
@@ -82,4 +81,3 @@ STACK_IRQM_END: .long __STACK_END__
 /* Semaphore tables space */
 
 SEMA_TABLES: .space 440
-
