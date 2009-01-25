14450a2a4a9d5f72505e0ef412b5928d1d70cf67
romana.koprivec <romana.koprivec@4fa3629e-ac03-11dd-a5cd-697bfd0a0cef>
2009-01-25 21:40:39 +0000

    Skip initialization of the Periodic Interval Timer if priority sheduler is used

--- branches/group6/src/tasks/task2.s
+++ branches/group6/src/tasks/task2.s
@@ -5,9 +5,14 @@
 
 task2:
 
-  /* Delay 200 ms */
-  mov r0, #200
-  swi #SYS_DELAY
+  ldr r3,=24000000           
+  do_something:                      
+     subs r3,r3,#1
+  bne do_something 
+  
+  /* Delay 25 ms */
+  mov r0, #25
+  swi #SYS_DELAY 
  
   b task2
 
--- branches/group6/src/tasks/task3.s
+++ branches/group6/src/tasks/task3.s
@@ -4,10 +4,21 @@
 .include "include/structures.s"
 
 task3:
-     
+
   /* Turn LED OFF */
   mov r0, #0
   swi #SYS_LED 
+  
+  ldr r3,=2400000            
+  do_something:                      
+     subs r3,r3,#1
+  bne do_something 
+  
+  /* Delay 300 ms */
+  mov r0, #300
+  swi #SYS_DELAY 
+
+
 
   b task3
 
